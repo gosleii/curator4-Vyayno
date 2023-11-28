613db33b4fab73e5880f141d5d44ec7becef0dd5
Pavel Sura <sleii@inbox.ru>
2023-11-28 14:07:16 +0300

    1

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 import telebot
 
-bot = telebot.TeleBot('6174644288:AAHRP-Rh9YqSWpQn4qnGFMWnpiyTaMhaJqs')
+bot = telebot.TeleBot('1125811332:AAGMQFHCEHV1Lswxr7aodPbgpyhVkQXVP6k')
 
 
 @bot.message_handler(commands=['start'])
 def main(message):
-    bot.send_mesage(message.chat.id, 'Сообщение')
+    bot.send_message(message.chat.id, 'Сообщение')
 
 
 @bot.message_handler(commands=['run'])
 def main(message):
-    bot.send_mesage(message.chat.id, '*Бежим!* \n Бежииим создавать бота!', parse_mode='Markdown')
+    bot.send_message(message.chat.id, '*Бежим!* \n Бежииим создавать бота!', parse_mode='Markdown')
 
 
 @bot.message_handler(commands=['pashalka'])
 def main(message):
-    bot.send_mesage(message.chat.id, '[ссылка] (https://youtu.be/dQw4w9WgXcQ?si=6vUBgU_roQX9IpGK)',
-                    parse_mode='Markdown')
+    bot.send_message(message.chat.id, "[ссылка](https://youtu.be/dQw4w9WgXcQ?si=6vUBgU_roQX9IpGK)", parse_mode='Markdown')
 
 
 bot.infinity_polling()
